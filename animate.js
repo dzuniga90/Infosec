7d659592eee1cc37ec52b88b20c346e5a270d42e
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-05-07 20:42:28 -0500

    Create animate.js

---
+++ animate.js
@@ -0,0 +1,13 @@
+/* $(document).ready(function() {
+	
+	// runs when an h2 heading is clicked
+    $("#common_attacks").click(function() {
+		$(this).toggleClass("minus");
+		$(this).next().stop(true).slideToggle(1000);
+
+    }); // end click
+    
+    // runs when the page is ready
+    $("#common_attacks").animate( { fontSize: "300%", opacity: 1, left: "+=375" }, 2000, "easeInCirc")  
+		         .animate( { fontSize: "175%", left: "-=200" }, 2000, "easeOutCirc");		    
+    }); // end ready */
